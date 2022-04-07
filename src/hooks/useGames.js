406d095eb7295010a06575b404a2be20f11af593
Dimash <dimash.k@numeto.com>
2022-04-07 23:50:50 +0200

    Fix(cards): correct the target links

--- src/hooks/useGames.js
+++ src/hooks/useGames.js
@@ -1,39 +1,38 @@
-    import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
+export function useGames(maxSize) {
+  const [games, setGames] = useState([]);
+  function shuffle(array) {
+    array.sort(() => Math.random() - 0.5);
+  }
+  useEffect(() => {
+    (async () => {
+      const card1 = new Array(33).fill({
+        url: "./img.jpeg",
+        link: "https://www.google.com/",
+      });
+      const card2 = new Array(33).fill({
+        url: "./img2.jpeg",
+        link: "https://www.google.com/",
+      });
+      const card3 = new Array(34).fill({
+        url: "./img3.jpeg",
+        link: "https://www.google.com/",
+      });
+      const cards = [...card1, ...card2, ...card3];
+      shuffle(cards);
+      const firstHalf = cards.slice(0, 54);
+      const secondHalf = cards.slice(54, 98);
+      const sprite = [
+        {
+          link: "https://www.google.com/",
+          url: "./sprite.png",
+          huge: true,
+        },
+      ];
+      setGames([...firstHalf, ...sprite, ...secondHalf]);
+    })();
+  }, [maxSize, setGames]);
 
-    export function useGames(maxSize) {
-      const [games, setGames] = useState([]);
-      function shuffle(array) {
-        array.sort(() => Math.random() - 0.5);
-      }
-      useEffect(() => {
-        (async () => {
-          const card1 = new Array(33).fill({
-            url: "./img.jpeg",
-            link: "google.com",
-          });
-          const card2 = new Array(33).fill({
-            url: "./img2.jpeg",
-            link: "google.com",
-          });
-          const card3 = new Array(34).fill({
-            url: "./img3.jpeg",
-            link: "google.com",
-          });
-          const cards = [...card1,...card2,...card3];
-          shuffle(cards);
-          const firstHalf = cards.slice(0,54);
-          const secondHalf =cards.slice(54, 98);
-          const sprite = [
-            {
-              link: "/google",
-              url: "./sprite.png",
-              huge: true,
-            },
-          ];
-          setGames([...firstHalf, ...sprite, ...secondHalf]);
-        })();
-      }, [maxSize, setGames]);
-
-      return games;
-    }
+  return games;
+}
